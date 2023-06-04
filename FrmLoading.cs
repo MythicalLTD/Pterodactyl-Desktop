6986e0f0b40d5f88baac3cc1938352ab857e5d73
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2023-06-04 19:28:51 +0200

    V0.0.0.5
    Added update check

---
+++ FrmLoading.cs
@@ -0,0 +1,123 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PteroController
+{
+    public partial class FrmLoading : Form
+    {
+        public DiscordRpcClient client;
+
+        public FrmLoading()
+        {
+            InitializeComponent();
+            InitializeRPC();
+        }
+
+        private void lblexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private async void CheckForUpdate()
+        {
+            string url = "https://raw.githubusercontent.com/MythicalLTD/PteroController/develop/Properties/AssemblyInfo.cs";
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    string assemblyInfoContent = await httpClient.GetStringAsync(url);
+                    string versionPattern = @"AssemblyFileVersion\(""(.*?)""\)";
+                    Match match = Regex.Match(assemblyInfoContent, versionPattern);
+                    if (match.Success)
+                    {
+                        string versionValue = match.Groups[1].Value;
+                        if (versionValue != Application.ProductVersion)
+                        {
+                            DialogResult result = MessageBox.Show("A new update is available. Do you want to install it?", "Update Available", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                            if (result == DialogResult.Yes)
+                            {
+                                FrmInstall x = new FrmInstall();
+                                x.Show();
+                                
+                                this.Hide();
+                            }
+                            else
+                            {
+                                Application.Exit();
+                            }
+                        }
+                        else
+                        {
+                            tm.Enabled = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Version not found in AssemblyInfo.cs");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+        private void FrmLoading_Load(object sender, EventArgs e)
+        {
+           CheckForUpdate();
+        }
+
+        void InitializeRPC()
+        {
+            client = new DiscordRpcClient("1114229771426078790");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.OnReady += (sender, e) =>
+            {
+            };
+            client.OnPresenceUpdate += (sender, e) =>
+            {
+            };
+            client.Initialize();
+
+            DiscordRPC.Button btns = new DiscordRPC.Button();
+            DiscordRPC.Button btns2 = new DiscordRPC.Button();
+            btns.Label = "Pterodactyl";
+            btns.Url = "https://pterodactyl.io";
+            btns2.Label = "Desktop app";
+            btns2.Url = "https://github.com/MythicalLTD/PteroController";
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = "Using PteroController",
+                State = "A Pterodactyl Panel Desktop App! <3",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logo",
+                    LargeImageText = "PteroController",
+                },
+                Buttons = new DiscordRPC.Button[]
+                {
+                    btns,
+                    btns2
+                },
+            });
+        }
+
+        private void tm_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 3;
+
+            if (panel2.Width >= 580)
+            {
+                tm.Stop();
+                FrmLogin x = new FrmLogin();
+                x.Show();
+                this.Hide();
+            }
+        }
+    }
+}
