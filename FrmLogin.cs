c1ac6b6cdc0d5165a208d9ef06fb982fcd5d9fcf
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2023-09-08 16:19:29 +0200

    Push

---
+++ FrmLogin.cs
@@ -0,0 +1,14 @@
+namespace PteroController;
+
+public partial class FrmLogin : Form
+{
+    public FrmLogin()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+}
